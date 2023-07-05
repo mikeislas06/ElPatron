1c3a1548e87e7f9014e7e19a36cad6393896030f
Miguel Islas <isgromiguel@gmail.com>
2023-07-05 13:04:54 -0600

    Agregando el Footer en la página de Reservaciones

--- src/components/ReservationsPage/ReservationsPage.jsx
+++ src/components/ReservationsPage/ReservationsPage.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './ReservationsPage.scss';
 import InformationSection from './InformationSection/InformationSection';
 import FormSection from './FormSection/FormSection';
+import FooterSection from '../common/FooterSection/FooterSection';
 
 const ReservationsPage = () => {
 	return (
 		<>
 			<InformationSection />
 			<FormSection />
+			<FooterSection />
 		</>
 	);
 };
